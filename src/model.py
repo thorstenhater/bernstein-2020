efb3071dc8f2921b6908e935c034910241f3adac
Brent Huisman <brent@huisman.pl>
2020-09-29 13:43:41 +0200

    quotes

--- src/model.py
+++ src/model.py
@@ -15,20 +15,20 @@ defaults, regions, ions, mechanisms = utils.load_allen_fit('fit.json')
 cell.set_properties(tempK=defaults.tempK, Vm=defaults.Vm,
                     cm=defaults.cm, rL=defaults.rL)
 for region, vs in regions:
-    cell.paint(region, tempK=vs.tempK, Vm=vs.Vm, cm=vs.cm, rL=vs.rL)
+    cell.paint('"'+region+'"', tempK=vs.tempK, Vm=vs.Vm, cm=vs.cm, rL=vs.rL)
 # set reversal potentials
 for region, ion, e in ions:
-    cell.paint(region, arb.ion(ion, rev_pot=e))
+    cell.paint('"'+region+'"', ion, rev_pot=e)
 cell.set_ion('ca', int_con=5e-5, ext_con=2.0, method=arb.mechanism('nernst/x=ca'))
 # assign ion dynamics
 for region, mech, values in mechanisms:
-    cell.paint(region, arb.mechanism(mech, values))
+    cell.paint('"'+region+'"', arb.mechanism(mech, values))
 # !\circled{5}! attach stimulus and spike detector
-cell.place('center', arb.iclamp(200, 1000, 0.15))
-cell.place('center', arb.spike_detector(-40))
+cell.place('"center"', arb.iclamp(200, 1000, 0.15))
+cell.place('"center"', arb.spike_detector(-40))
 # !\circled{6}! set up runnable simulation
 model = arb.single_cell_model(cell)
-model.probe('voltage', 'center', frequency=200000) # see !\circled{5}!
+model.probe('voltage', '"center"', frequency=200000) # see !\circled{5}!
 # !\circled{7}! assign catalogues
 model.properties.catalogue = arb.allen_catalogue()
 model.properties.catalogue.extend(arb.default_catalogue(), '')
